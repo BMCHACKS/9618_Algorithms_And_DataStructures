b43555dafae9791597cd0de832e242a360fba79c
BMCHACKS <30594382+BMCHACKS@users.noreply.github.com>
2025-05-06 03:55:06 +0500

    Update stack.py

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -42,7 +42,7 @@ def pop() -> int | str:
     # first check if stack is empty or not
 
     return_data = 0 # or "" depending on the data stored in the stack
-    if Head == 0: return retrun_data
+    if Head == 0: return return_data
 
     Head -= 1
     return_data = stack[Head]
