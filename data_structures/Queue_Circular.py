7f59bc326e85cf64d0ccf411c0784d4b33056249
BMCHACKS <30594382+BMCHACKS@users.noreply.github.com>
2026-02-12 20:20:45 +0500

    Added linear and circular queue codes

---
+++ data_structures/Queue_Circular.py
@@ -0,0 +1,48 @@
+
+def Enqueue(value):
+    global Head, Tail, Free
+    if Free == 0:
+        print("There is no more space in the Q!")
+    else:
+        CircularQueue[Tail] = value
+        Tail += 1
+        Free -= 1
+        if Head == -1:
+            Head = 0
+        if Tail > len(CircularQueue)-1:
+            Tail = 0
+
+def Dequeue():
+    global Head, Tail, Free
+    if Free == 7:
+        print("Queue is empty! - Nothing to remove!")
+        return ""
+    else:
+        Free += 1
+        value = CircularQueue[Head]
+        CircularQueue[Head] = ""
+        Head += 1
+        if Head > len(CircularQueue)-1:
+            Head = 0
+        if Free == 7:
+            Head = -1
+            Tail = 0
+        return value
+
+Size          = 7
+CircularQueue = ["" for _ in range(Size)]
+Head          = -1 # Points towards the FIRST person in the q
+Tail          = 0  # Points towards the NEXT FREE SPACE in a q
+Free          = len(CircularQueue) # current amount of free spaces in the q
+
+# --- below code for testing the code only ---
+for x in range(20):
+    choice = input("Enter 1 to enqueue, 2 to dequeue: ")
+    if choice == "1":
+        value = input("Enter a name to enqueue: ")
+        Enqueue(value)
+    elif choice == "2":
+        value = Dequeue()
+        if value != "":
+            print(f"Dequeued : {value}")
+    print(CircularQueue)
---
+++ data_structures/linear_queue.py
@@ -0,0 +1,42 @@
+
+def Enqueue(value):
+    global Head, Tail
+    if Tail == len(LinearQueue):
+        print("There is no more space in the Q!")
+    else:
+        LinearQueue[Tail] = value
+        Tail += 1
+        if Head == -1:
+            Head = 0
+
+def Dequeue():
+    global Head, Tail
+    if Head == -1:
+        print("Queue is empty! - Nothing to remove!")
+        return ""
+    else:
+        value = LinearQueue[Head]
+        LinearQueue[Head] = ""
+        Head += 1
+        if Head == Tail:
+            Head = -1
+            Tail = 0
+        return value
+
+Size        = 7
+LinearQueue = ["" for _ in range(size)]
+Head        = -1 # Points towards the FIRST person in the q
+Tail        = 0  # Points towards the NEXT FREE SPACE in a q
+
+
+# --- below code for testing the code only ---
+for x in range(20):
+    choice = input("Enter 1 to enqueue, 2 to dequeue: ")
+    if choice == "1":
+        value = input("Enter a name to enqueue: ")
+        Enqueue(value)
+    elif choice == "2":
+        value = Dequeue()
+        if value != "":
+            print(f"Dequeued : {value}")
+    print(LinearQueue)
